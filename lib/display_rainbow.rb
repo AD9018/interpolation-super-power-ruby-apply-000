a319b9a89896d9b8d4f5ed1da0b4d42f0f1d48d8
Adam Dadi <adamdadi23@gmail.com>
2020-10-09 20:49:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 def display_rainbow
 display_rainbows(['red','orange','yellow','green','blue','indigo','violet'])
 colors="R:red,O:orange,Y:yellow,G:green,B:blue,I:indigo,v:violet"
-puts "The first color in the rainbow is: #{display_rainbows[0]}"
-puts "The second color in the rainbow is #{display_rainbows[1]}"
-puts "The third color in the rainbow is #{display_rainbows[2]}"
-puts "The fourth color in the rainbow is #{display_rainbows[3]}"
-puts "The fifth color in the rainbow is #{display_rainbows[4]}"
-puts "The sixth color in the rainbow is #{display_rainbows[5]}"
-puts "The seventh color in the rainbow is #{display_rainbows[6]}"
+puts "The first color in the rainbow is: #{display_rainbows[0,red]}"
+puts "The second color in the rainbow is #{display_rainbows[1,orange]}"
+puts "The third color in the rainbow is #{display_rainbows[2,yellow]}"
+puts "The fourth color in the rainbow is #{display_rainbows[3,green]}"
+puts "The fifth color in the rainbow is #{display_rainbows[4,blue]}"
+puts "The sixth color in the rainbow is #{display_rainbows[5,indigo]}"
+puts "The seventh color in the rainbow is #{display_rainbows[6,violet]}"
 end
