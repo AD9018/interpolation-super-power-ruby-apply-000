b17ced519a8242eefdad52db18171ed68e505d8a
Adam Dadi <adamdadi23@gmail.com>
2020-10-09 20:47:35 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,11 @@
 def display_rainbow
 display_rainbows(['red','orange','yellow','green','blue','indigo','violet'])
 colors="R:red,O:orange,Y:yellow,G:green,B:blue,I:indigo,v:violet"
+puts "The first color in the rainbow is: #{display_rainbows[0]}"
+puts "The second color in the rainbow is #{display_rainbows[1]}"
+puts "The third color in the rainbow is #{display_rainbows[2]}"
+puts "The fourth color in the rainbow is #{display_rainbows[3]}"
+puts "The fifth color in the rainbow is #{display_rainbows[4]}"
+puts "The sixth color in the rainbow is #{display_rainbows[5]}"
+puts "The seventh color in the rainbow is #{display_rainbows[6]}"
 end
