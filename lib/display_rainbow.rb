d4d96ada26069be788b77ce897ef92f11f93f683
Adam Dadi <adamdadi23@gmail.com>
2020-10-09 21:07:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,12 @@
 def display_rainbow(argument)
-colors="R:red,O:orange,Y:yellow,G:green,B:blue,I:indigo,v:violet"
-puts "The first color in the rainbow is: #{display_rainbows[0,red]}"
-puts "The second color in the rainbow is #{display_rainbows[1,orange]}"
-puts "The third color in the rainbow is #{display_rainbows[2,yellow]}"
-puts "The fourth color in the rainbow is #{display_rainbows[3,green]}"
-puts "The fifth color in the rainbow is #{display_rainbows[4,blue]}"
-puts "The sixth color in the rainbow is #{display_rainbows[5,indigo]}"
-puts "The seventh color in the rainbow is #{display_rainbows[6,violet]}"
+  puts "The first color in the rainbow is: #{display_rainbows[0,red]}"
+  puts "The second color in the rainbow is #{display_rainbows[1,orange]}"
+  puts "The third color in the rainbow is #{display_rainbows[2,yellow]}"
+  puts "The fourth color in the rainbow is #{display_rainbows[3,green]}"
+  puts "The fifth color in the rainbow is #{display_rainbows[4,blue]}"
+  puts "The sixth color in the rainbow is #{display_rainbows[5,indigo]}"
+  puts "The seventh color in the rainbow is #{display_rainbows[6,violet]}"
 end
 
+#display_rainbows(['red','orange','yellow','green','blue','indigo','violet'])
+#colors="R:red,O:orange,Y:yellow,G:green,B:blue,I:indigo,v:violet"
